83b0c7fc5b5e9e517262986cecaad832ae729e67
carlazitroc <maricarl.ortiz@gmail.com>
2016-07-15 01:37:59 +0800

    Update Layour

--- index.php
+++ index.php
@@ -141,6 +141,30 @@ include('header.html');
     </div>
 
     <a id="back-to-top" href="#page-top" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-2 col-lg-offset-3 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>+852 2236 5696</p>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <i class="fa fa-fax fa-3x sr-contact"></i>
+                    <p>+852 2236 5611</p>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">admin@masguru.co</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 <?php 
 
